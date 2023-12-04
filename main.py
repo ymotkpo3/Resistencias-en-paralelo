718a8022c374bc9e41ae07cee97d6c13b42ef2ac
ymotkpo3 <76632004+ymotkpo3@users.noreply.github.com>
2023-12-04 20:36:09 -0300

    Update main.py
    hecho para sufrir

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import sys
-
-
 def obtener_resistencias():
     resistencias = []
     while True:
@@ -13,30 +11,18 @@ def obtener_resistencias():
         except ValueError:
             print("Por favor, ingrese un número válido.")
     return resistencias
-
-
 def calcular_resistencia_total(resistencias):
     if not resistencias:
         sys.exit("No se ingresaron resistencias. Terminando programa.")
-
     resistencias_inversas = [1 / r for r in resistencias]
     resistencia_total = 1 / sum(resistencias_inversas)
-
     return resistencia_total
-
-
 def main():
     print("¡Bienvenido a la CALCULADORA DE RESISTENCIAS EN PARALELO!")
-
     if input("¿Desea comenzar? [S] [N]: ").upper() != "S":
         sys.exit("Se terminó la ejecución del programa.")
-
     resistencias = obtener_resistencias()
-
     resistencia_total = calcular_resistencia_total(resistencias)
-
     print(f"La resistencia total es de {resistencia_total:.2f} ohms")
-
-
 if __name__ == "__main__":
     main()
